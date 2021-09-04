fdbfdae48d18a0dec8d4f657146b017b2c65460b
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-09-04 09:21:03 +0200

    116. Adding a migration for seeding restrictions

---
+++ migrations/20210904071646_seed_restriction_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.restriction (id, restriction_name, created_at, updated_at) VALUES (1, 'Reservation', '2021-08-30 23:36:48.000000', '2021-08-30 23:36:51.000000');
+INSERT INTO public.restriction (id, restriction_name, created_at, updated_at) VALUES (2, 'Owner Block', '2021-08-30 23:36:48.000000', '2021-08-30 23:36:51.000000');
