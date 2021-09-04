c00aab52376d16de1676d3e09912bfc811392129
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-09-04 09:14:45 +0200

    115. Adding a migration for seeding rooms

---
+++ migrations/20210904071039_seed_room_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.room (id, room_name, created_at, updated_at) VALUES (1, 'General''s Quarters', '2021-08-30 23:36:48.000000', '2021-08-30 23:36:51.000000');
+INSERT INTO public.room (id, room_name, created_at, updated_at) VALUES (2, 'Major''s Suite', '2021-09-01 22:33:25.000000', '2021-09-01 22:33:25.000000');
